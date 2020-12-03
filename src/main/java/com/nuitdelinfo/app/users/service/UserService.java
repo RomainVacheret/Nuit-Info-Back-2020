dbaee5c2567c66fb9e3587a7dfe09a4f73206b59
Trystan4 <trystan.rcs4@gmail.com>
2020-12-03 22:48:39 +0100

    feat: add user service implementation

--- src/main/java/com/nuitdelinfo/app/users/service/UserService.java
+++ src/main/java/com/nuitdelinfo/app/users/service/UserService.java
@@ -1,4 +1,21 @@
 package com.nuitdelinfo.app.users.service;
 
+import com.nuitdelinfo.app.model.Group;
+import com.nuitdelinfo.app.model.User;
+
 public interface UserService {
+
+    void connection(User user);
+    void deconnection(User user);
+
+    void addFriend(User friend, User user);
+    void deleteFriend(User user,String friendName);
+
+    void modifyName(User user,String name);
+    void modifyLastName(User user,String lastName);
+    void modifyPseudo(User user,String pseudo);
+    void modifyEmail(User user,String email);
+
+    void addGroup(User user,Group group);
+    void deleteGroup(User user,Group group);
 }
