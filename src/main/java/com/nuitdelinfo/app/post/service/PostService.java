15b41c69e3c658b3787ea9819611b7f1451b8718
Trystan4 <trystan.rcs4@gmail.com>
2020-12-04 03:54:04 +0100

    feat: add post services and post controller

--- src/main/java/com/nuitdelinfo/app/post/service/PostService.java
+++ src/main/java/com/nuitdelinfo/app/post/service/PostService.java
@@ -1,19 +1,29 @@
 package com.nuitdelinfo.app.post.service;
 
 import java.util.Optional;
+import java.util.Set;
 
+import com.nuitdelinfo.app.model.Comment;
 import com.nuitdelinfo.app.model.Post;
+import com.nuitdelinfo.app.model.UGroup;
+import com.nuitdelinfo.app.model.User;
 
 public interface PostService {
     void modifyName(Optional<Post> post, String name);
-    void addGroup(Optional<Post> post, Long idg);
-    void addUser(Optional<Post> post, Long id);
+    void modifyGroup(Optional<Post> post, Optional<UGroup> group);
+    void modifyUser(Optional<Post> post,Optional<User> user);
 
-    void addDescription(Optional<Post> post, String description);
-    void deleteDescription(Optional<Post> post, String description);
+    void modifyDescription(Optional<Post> post, String description);
+    void deleteDescription(Optional<Post> post);
 
-    void addComment(Optional<Post> post,Long idc);
-    void deleteComment(Optional<Post> post,Long idc);
+    void addComment(Optional<Post> post,Optional<Comment> comment);
+    void deleteComment(Optional<Post> post, Long idc);
     
     Optional<Post> getByID(Long id);
+
+    String displayName(Optional<Post> post);
+    UGroup displayGroup(Optional<Post> post);
+    User displayUser(Optional<Post> post);
+    String displayDescription(Optional<Post> post);
+    Set<Comment> displayComments(Optional<Post> post);
 }
--- src/main/java/com/nuitdelinfo/app/post/service/PostServiceImpl.java
+++ src/main/java/com/nuitdelinfo/app/post/service/PostServiceImpl.java
@@ -1,8 +1,13 @@
 package com.nuitdelinfo.app.post.service;
 
+import java.util.HashSet;
 import java.util.Optional;
+import java.util.Set;
 
+import com.nuitdelinfo.app.model.Comment;
 import com.nuitdelinfo.app.model.Post;
+import com.nuitdelinfo.app.model.UGroup;
+import com.nuitdelinfo.app.model.User;
 import com.nuitdelinfo.app.post.repository.PostRepository;
 
 import org.springframework.stereotype.Service;
@@ -20,46 +25,91 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public void addGroup(Optional<Post> post, Long idg) {
-        // TODO Auto-generated method stub
+	public Optional<Post> getByID(Long id) {
+		return repository.findById(id);
+	}
+
+    @Override
+    public void modifyGroup(Optional<Post> post, Optional<UGroup> group) {
+        if(post.isPresent() && group.isPresent())
+            post.get().setGroup(group.get());
 
     }
 
     @Override
-    public void addUser(Optional<Post> post, Long id) {
-        // TODO Auto-generated method stub
+    public void modifyUser(Optional<Post> post, Optional<User> user) {
+        if(post.isPresent() && user.isPresent())
+            post.get().setUser(user.get());
 
     }
 
     @Override
-    public void addDescription(Optional<Post> post, String description) {
-        // TODO Auto-generated method stub
+    public void modifyDescription(Optional<Post> post, String description) {
+        if(post.isPresent())
+            post.get().setDescription(description);
 
     }
 
     @Override
-    public void deleteDescription(Optional<Post> post, String description) {
-        // TODO Auto-generated method stub
+    public void deleteDescription(Optional<Post> post) {
+        if(post.isPresent())
+            post.get().setDescription("");
 
     }
 
     @Override
-    public void addComment(Optional<Post> post, Long idc) {
-        // TODO Auto-generated method stub
+    public void addComment(Optional<Post> post, Optional<Comment> comment) {
+        if(post.isPresent() && comment.isPresent())
+            post.map(p -> p.getComments().add(comment.get()));
 
     }
 
     @Override
     public void deleteComment(Optional<Post> post, Long idc) {
-        // TODO Auto-generated method stub
+        if(post.isPresent()){
+            post.get().getComments().forEach(c ->{
+                if(c.getId() == idc)
+                    post.get().getComments().remove(c);
+            });
+        }
 
     }
 
-    
     @Override
-	public Optional<Post> getByID(Long id) {
-		return repository.findById(id);
-	}
+    public String displayName(Optional<Post> post) {
+        if(post.isPresent())
+            return post.get().getName();
+        return null;
+    }
+
+    @Override
+    public UGroup displayGroup(Optional<Post> post) {
+        if(post.isPresent())
+            return post.get().getGroup();
+        return null;
+    }
+
+    @Override
+    public User displayUser(Optional<Post> post) {
+        if(post.isPresent())
+            return post.get().getUser();
+        return null;
+    }
+
+    @Override
+    public String displayDescription(Optional<Post> post) {
+        if(post.isPresent())
+            return post.get().getDescription();
+        return null;
+    }
+
+    @Override
+    public Set<Comment> displayComments(Optional<Post> post) {
+        Set<Comment> rstNull = new HashSet<>();
+        if(post.isPresent())
+            return post.get().getComments();
+        return rstNull;
+    }
 
     
 }
